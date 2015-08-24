fb4e09d4d5819c3a67d36e06ca625d62e0009ac2
William Creelman <william.creelman@ocdsb.ca>
2015-08-24 14:06:02 -0400

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+// my first program in C++
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
+
+OR
+
+// my first program in C++
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+}
