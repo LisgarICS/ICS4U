2046a13e50c95fe701e652ca13c4c643d95652de
William Creelman <william.creelman@ocdsb.ca>
2016-10-24 10:45:15 -0400

    Create Murakami.h

---
+++ Ch13/Murakami/Murakami.h
@@ -0,0 +1,17 @@
+// Murakami.h  
+#include <iostream>
+#include <apstring.h>
+
+class Murakami {
+	private:
+		apstring title;
+		bool read;
+	public:
+		Murakami() {title = ""; read = false;}
+		Murakami(apstring t) {title = t; read = false;}
+		void setTitle(apstring t) {title = t;}
+		void setRead() {read = true;}
+		void QTheTitle();
+		void print();	
+		~Murakami() {std::cout << "book out of slope" << std::endl;}
+};
