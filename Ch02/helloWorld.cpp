e73b7cb891c8847b720842241a36a1049b593da3
William Creelman <william.creelman@ocdsb.ca>
2017-01-12 11:24:14 -0500

    Create helloWorld.cpp

---
+++ Ch02/helloWorld.cpp
@@ -0,0 +1,18 @@
+// my first program in C++
+// demonstrates using the standard namespace explicitly 
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
+
+OR
+
+// my first program in C++
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+}
